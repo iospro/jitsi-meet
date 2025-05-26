9920dedd1c1749c12e1b7a36f541c1cfffcdfa54
Alexey Matveev <malex@1forma.ru>
2025-05-26 17:41:30 +0300

    - переработка кол-ва кнопок в нижнем меню, цвет кнопки hangup в native

--- react/features/toolbox/constants.ts
+++ react/features/toolbox/constants.ts
@@ -39,24 +39,20 @@ export const THRESHOLDS = [
  */
 export const NATIVE_THRESHOLDS = [
     {
-        width: 560,
+        width: 500,
         order: [ 'microphone', 'camera', 'chat', 'screensharing', 'raisehand', 'tileview', 'overflowmenu', 'hangup' ]
     },
     {
-        width: 500,
+        width: 428,
         order: [ 'microphone', 'camera', 'chat', 'raisehand', 'tileview', 'overflowmenu', 'hangup' ]
     },
     {
-        width: 440,
+        width: 375,
         order: [ 'microphone', 'camera', 'chat', 'raisehand', 'overflowmenu', 'hangup' ]
     },
     {
-        width: 380,
-        order: [ 'microphone', 'camera', 'chat', 'overflowmenu', 'hangup' ]
-    },
-    {
         width: 320,
-        order: [ 'microphone', 'camera', 'overflowmenu', 'hangup' ]
+        order: [ 'microphone', 'camera', 'chat', 'overflowmenu', 'hangup' ]
     }
 ];
 
